4f55bdd6ce9b31fd0492a60c1cb1caef619e9ea7
Eva del Carmen Gonzalez Santiz <L24270005@tuxtla.tecnm.mx>
2026-03-07 15:29:14 -0600

    Agregando animacion al indicador de carga

--- src/components/CountriesView.vue
+++ src/components/CountriesView.vue
@@ -4,20 +4,30 @@
 
   <h2>Lista de países del mundo</h2>
 
-  <!--Indicador de carga-->
-  <p v-if="loading">Cargando paises...</p>
+  <!--Campo de busqueda-->
+  <input type="text" placeholder="Buscar pais ..." v-model="busqueda" class="search">
 
-  <p v-if="!loading && paises.length === 0">
+  <!--USO DEL v-if-->
+  <!--Indicador de carga con v-if-->
+  <div v-if="loading || buscando" class="loader-container">
+    <div class="loader"></div>
+    <p>Cargando paises...</p>
+  </div>
+
+  <div v-if="!loading && !buscando && paisesFiltrados.length === 0" class="no-results">
     No hay paises para mostrar
-  </p>
+    <br>
+    Intenta escribir otro
 
-  <div class="countries" v-if="!loading && paises.length > 0">
+  </div>
+
+  <div class="countries" v-if="!loading && paisesFiltrados.length > 0">
 
     <!-- LISTA GENERADA CON v-for -->
 
     <div
       class="card"
-      v-for="pais in paises"
+      v-for="pais in paisesFiltrados"
       :key="pais.name.common"
     >
 
@@ -41,11 +51,12 @@
 
 
 <script setup>
-
-import { ref, onMounted } from "vue"
+import { ref, onMounted, computed, watch } from "vue"
 
 const paises = ref([])
 const loading = ref(true)
+const buscando = ref(false)
+const busqueda = ref("")
 
 onMounted(async () => {
 
@@ -59,10 +70,23 @@ onMounted(async () => {
   loading.value = false
 
 })
+/*Filtro de busqueda */
+const paisesFiltrados = computed (() => {
+  return paises.value.filter(pais =>
+    pais.name.common.toLowerCase().includes(busqueda.value.toLowerCase())
+  )
+})
+
+/*Tiempo de busqueda*/
+watch (busqueda, () => {
+  buscando.value = true
+
+  setTimeout(() => {
+    buscando.value = false
+  }, 2000)
+})
 
 </script>
-
-
 <style>
 
 .page{
@@ -77,10 +101,51 @@ h2{
   margin-bottom:30px;
 }
 
+.loader-container {
+  text-align: center;
+  margin-top: 30px;
+}
+
+.loader {
+  border: 6px solid #f3f3f3;
+  border-top: 6px solid #42b883;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+  margin: 0 auto 10px auto;
+}
+
+@keyframes spin{
+  0%{ transform: rotate(0deg);}
+  100%{ transform: rotate(360deg);}
+}
+
+.no-results{
+  text-align:center;
+  margin-top:40px;
+  font-size:18px;
+  background:#fff3f3;
+  color:#c0392b;
+  padding:20px;
+  border-radius:10px;
+  border:1px solid #f5c6c6;
+}
+
 .countries{
   display:grid;
   grid-template-columns:repeat(auto-fill,minmax(250px,1fr));
   gap:20px;
+}
+
+.search {
+  display: block;
+  margin: 0 auto 30px auto;
+  padding: 10px;
+  width: 300px;
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  font-size: 16px;
 }
 
 .card{

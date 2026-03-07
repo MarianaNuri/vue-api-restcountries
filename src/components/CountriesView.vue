2e9c601ce8196afccd867dbf6ae277c1e48d07f9
MarianaNuri <l24270017@tuxtla.tecnm.mx>
2026-03-06 19:21:00 -0600

    V-for y datos de paises

---
+++ src/components/CountriesView.vue
@@ -0,0 +1,98 @@
+<template>
+
+<div class="page">
+
+  <h2>Lista de países del mundo</h2>
+
+  <div class="countries">
+
+    <!-- LISTA GENERADA CON v-for -->
+
+    <div
+      class="card"
+      v-for="pais in paises"
+      :key="pais.name.common"
+    >
+
+      <img :src="pais.flags.png" />
+
+      <h3>{{ pais.name.common }}</h3>
+
+      <p><strong>Capital:</strong> {{ pais.capital?.[0] }}</p>
+
+      <p><strong>Región:</strong> {{ pais.region }}</p>
+
+      <p><strong>Población:</strong> {{ pais.population.toLocaleString() }}</p>
+
+    </div>
+
+  </div>
+
+</div>
+
+</template>
+
+
+<script setup>
+
+import { ref, onMounted } from "vue"
+
+const paises = ref([])
+
+onMounted(async () => {
+
+  const respuesta = await fetch(
+  "https://restcountries.com/v3.1/all?fields=name,flags,capital,region,subregion,population,languages,currencies"
+  )
+
+  const data = await respuesta.json()
+
+  paises.value = data
+
+})
+
+</script>
+
+
+<style>
+
+.page{
+  max-width:1200px;
+  margin:auto;
+  padding:40px;
+  font-family:Segoe UI;
+}
+
+h2{
+  text-align:center;
+  margin-bottom:30px;
+}
+
+.countries{
+  display:grid;
+  grid-template-columns:repeat(auto-fill,minmax(250px,1fr));
+  gap:20px;
+}
+
+.card{
+  background:white;
+  border-radius:12px;
+  padding:15px;
+  box-shadow:0 4px 10px rgba(0,0,0,0.1);
+  transition:0.3s;
+}
+
+.card:hover{
+  transform:translateY(-5px);
+}
+
+.card img{
+  width:100%;
+  border-radius:8px;
+}
+
+.card h3{
+  margin-top:10px;
+}
+
+</style>
